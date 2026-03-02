d568f3785fc46197c8ab9062ea274362147432a7
Cynthia Rey <cynthia.rey@irit.fr>
2026-03-02 11:18:52 +0100

    chore: docs, move storybook-generated files to the right place

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -2,8 +2,8 @@ import type { StorybookConfig } from '@storybook-vue/nuxt'
 
 const config: StorybookConfig = {
   stories: [
-    '../components/**/*.mdx',
-    '../components/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../app/components/**/*.mdx',
+    '../app/components/**/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
   addons: [
     '@storybook/addon-a11y',
