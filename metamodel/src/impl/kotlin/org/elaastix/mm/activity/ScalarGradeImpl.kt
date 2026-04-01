9673f26e254c90250ab4823df887d4313bb4c943
Cynthia Rey <cynthia.rey@irit.fr>
2026-04-01 17:44:20 +0200

    feat(mm/impl): scalar grade embeddable

---
+++ metamodel/src/impl/kotlin/org/elaastix/mm/activity/ScalarGradeImpl.kt
@@ -0,0 +1,46 @@
+/*
+ * Elaastic / ElaastiX - formative assessment system
+ * Copyright (C) 2019  Université de Toulouse and Université Toulouse Capitole.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.elaastix.mm.activity
+
+import jakarta.persistence.Embeddable
+
+@JvmRecord
+@Embeddable
+@PublishedApi
+@Suppress(
+    "RedundantModalityModifier", // all-open wants to make it open; explicit final is needed to get a Java Record.
+    "JpaEntityWithValAttributesInspection", // https://youtrack.jetbrains.com/issue/KTIJ-37754
+)
+internal final data class ScalarGradeImpl(
+    // Validation using Jakarta is too annoying (cross-field validation boilerplate is HUGE).
+    // Validated in the form of a smart constructor instead. Much less verbose, will still do the trick :)
+    override val grade: UInt,
+    override val max: UInt,
+) : ScalarGradable.ScalarGrade
+
+/**
+ * Factory to create an instance of [ScalarGradable.ScalarGrade].
+ */
+fun ScalarGrade(grade: UInt, max: UInt): ScalarGradable.ScalarGrade {
+    require(max != 0U) { "Maximum value of the grade must be strictly positive" }
+    require(grade <= max) { "Grade must be less than or equal to the maximum grade" }
+
+    return ScalarGradeImpl(grade, max)
+}
