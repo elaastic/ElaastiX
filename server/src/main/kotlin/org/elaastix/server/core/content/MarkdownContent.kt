f8ee41df758c29704d8e149e484fcbe96267a5d7
Cynthia Rey <cynthia.rey@irit.fr>
2026-04-28 17:42:31 +0200

    feat: implement basic content types

---
+++ server/src/main/kotlin/org/elaastix/server/core/content/MarkdownContent.kt
@@ -0,0 +1,40 @@
+/*
+ * Elaastic / ElaastiX - formative assessment system
+ * Copyright (C) 2019  Université de Toulouse and Université Toulouse Capitole.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.elaastix.server.core.content
+
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
+import org.elaastix.commons.utils.requireJsonString
+import org.elaastix.mm.content.FormattedContent
+
+/**
+ * Contents formatted in Markdown (Obsidian Flavoured).
+ */
+class MarkdownContent(
+	/** The Markdown content. */
+	val content: String,
+) : FormattedContent {
+	override fun toJson() = JsonPrimitive(content)
+
+	companion object : FormattedContent.Factory {
+		override fun fromJson(json: JsonElement): FormattedContent =
+			requireJsonString(json).let { MarkdownContent(json.content) }
+	}
+}
---
+++ server/src/main/kotlin/org/elaastix/server/core/content/MarkdownText.kt
@@ -0,0 +1,38 @@
+/*
+ * Elaastic / ElaastiX - formative assessment system
+ * Copyright (C) 2019  Université de Toulouse and Université Toulouse Capitole.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.elaastix.server.core.content
+
+import org.elaastix.mm.content.FormattedText
+
+/**
+ * Plaintext formatted in Markdown (inline syntax elements only).
+ *
+ * CommonMark (0.31.2) § 6 "Inlines" ONLY, excluding § 6.4 "Images", § 6.6 "Raw HTML", § 6.7 "Hard line breaks".
+ */
+class MarkdownText(
+	/** The Markdown text. */
+	val content: String,
+) : FormattedText {
+	override fun toString() = content
+
+	companion object : FormattedText.Factory {
+		override fun fromString(string: String) = MarkdownText(string)
+	}
+}
