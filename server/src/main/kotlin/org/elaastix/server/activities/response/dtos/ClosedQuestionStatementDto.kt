49a4bf9b1c63eead87a6b426f748752a10218397
Cynthia Rey <cynthia.rey@irit.fr>
2026-04-29 16:32:20 +0200

    feat: question projection and DTOs

---
+++ server/src/main/kotlin/org/elaastix/server/activities/response/dtos/ClosedQuestionStatementDto.kt
@@ -0,0 +1,43 @@
+/*
+ * Elaastic / ElaastiX - formative assessment system
+ * Copyright (C) 2019  Université de Toulouse and Université Toulouse Capitole.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.elaastix.server.activities.response.dtos
+
+import kotlinx.serialization.Serializable
+import org.elaastix.commons.data.Uuid
+import org.elaastix.mm.content.FormattedText
+import org.elaastix.mm.content.RichContent
+
+/**
+ * A closed question, without the answer.
+ */
+@Serializable
+data class ClosedQuestionStatementDto(
+	/** The question's unique identifier. */
+	val id: Uuid,
+
+	/** The question's statement. */
+	val statement: RichContent,
+
+	/** Whether the question accepts multiple answers, or only a single one. */
+	val multiple: Boolean,
+
+	/** List of choices the user can pick from. Ordered as authored by the author. */
+	val choices: List<FormattedText>,
+) : QuestionStatementDto
