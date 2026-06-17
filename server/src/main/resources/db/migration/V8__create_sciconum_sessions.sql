882b1ad6af78fc8b17298af6df95864f4392e1a9
Cynthia Rey <cynthia.rey@irit.fr>
2026-06-17 13:13:30 +0200

    chore: migration

--- server/src/main/resources/db/migration/V8__create_sciconum_sessions.sql
+++ server/src/main/resources/db/migration/V8__create_sciconum_sessions.sql
@@ -17,6 +17,16 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+CREATE TABLE sciconum_chat_peering_entity
+(
+	id                  UUID                        NOT NULL,
+	updated_at          TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+	version             BIGINT                      NOT NULL,
+	scenario_session_id UUID                        NOT NULL,
+	session_round       INTEGER                     NOT NULL,
+	CONSTRAINT pk_sciconumchatpeeringentity PRIMARY KEY (id)
+);
+
 CREATE TABLE sciconum_chatter_entity
 (
 	id         UUID                        NOT NULL,
@@ -28,81 +38,79 @@ CREATE TABLE sciconum_chatter_entity
 	CONSTRAINT pk_sciconumchatterentity PRIMARY KEY (id)
 );
 
-CREATE TABLE sciconum_learner_session_entity
+CREATE TABLE sciconum_judge_peering_entity
 (
-	id                UUID                        NOT NULL,
-	updated_at        TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-	version           BIGINT                      NOT NULL,
-	global_session_id UUID                        NOT NULL,
-	learner_id        UUID                        NOT NULL,
-	phase             VARCHAR(255)                NOT NULL,
-	next_phase_at     TIMESTAMP WITHOUT TIME ZONE,
-	CONSTRAINT pk_sciconumlearnersessionentity PRIMARY KEY (id)
+	id                 UUID                        NOT NULL,
+	updated_at         TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+	version            BIGINT                      NOT NULL,
+	learner_session_id UUID                        NOT NULL,
+	session_round      INTEGER                     NOT NULL,
+	CONSTRAINT pk_sciconumjudgepeeringentity PRIMARY KEY (id)
 );
 
-CREATE TABLE sciconum_peering_entity
-(
-	id         UUID                        NOT NULL,
-	dtype      VARCHAR(31)                 NOT NULL,
-	updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-	version    BIGINT                      NOT NULL,
-	session_id UUID                        NOT NULL,
-	round      INTEGER                     NOT NULL,
-	learner_id UUID                        NOT NULL,
-	CONSTRAINT pk_sciconumpeeringentity PRIMARY KEY (id)
-);
-
-CREATE TABLE sciconum_peering_entity_responses
+CREATE TABLE sciconum_judge_peering_entity_responses
 (
 	sciconum_judge_peering_entity_id UUID NOT NULL,
 	responses_id                     UUID NOT NULL
 );
 
-CREATE TABLE sciconum_session_entity
+CREATE TABLE sciconum_learner_session_entity
 (
-	id               UUID                        NOT NULL,
-	updated_at       TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-	version          BIGINT                      NOT NULL,
-	assignment_id    UUID                        NOT NULL,
-	sequence_id      UUID                        NOT NULL,
-	current_question INTEGER                     NOT NULL,
-	phase            VARCHAR(255)                NOT NULL,
-	next_phase_at    TIMESTAMP WITHOUT TIME ZONE,
-	CONSTRAINT pk_sciconumsessionentity PRIMARY KEY (id)
+	id                  UUID                        NOT NULL,
+	updated_at          TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+	version             BIGINT                      NOT NULL,
+	scenario_session_id UUID                        NOT NULL,
+	learner_id          UUID                        NOT NULL,
+	phase               VARCHAR(255)                NOT NULL,
+	next_phase_at       TIMESTAMP WITHOUT TIME ZONE,
+	CONSTRAINT pk_sciconumlearnersessionentity PRIMARY KEY (id)
 );
 
-ALTER TABLE sciconum_session_entity
-	ADD CONSTRAINT uc_0049f1a1a62c78bb4160627ee UNIQUE (assignment_id, sequence_id);
+CREATE TABLE sciconum_scenario_session_entity
+(
+	id            UUID                        NOT NULL,
+	updated_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+	version       BIGINT                      NOT NULL,
+	assignment_id UUID                        NOT NULL,
+	sequence_id   UUID                        NOT NULL,
+	current_round INTEGER                     NOT NULL,
+	phase         VARCHAR(255)                NOT NULL,
+	next_phase_at TIMESTAMP WITHOUT TIME ZONE,
+	CONSTRAINT pk_sciconumscenariosessionentity PRIMARY KEY (id)
+);
+
+ALTER TABLE sciconum_scenario_session_entity
+	ADD CONSTRAINT uc_73f406295bda0b595d035dc5e UNIQUE (assignment_id, sequence_id);
 
 ALTER TABLE sciconum_chatter_entity
 	ADD CONSTRAINT unique_nick_per_group UNIQUE (peering_id, nickname);
+
+ALTER TABLE sciconum_chat_peering_entity
+	ADD CONSTRAINT FK_SCICONUMCHATPEERINGENTITY_ON_SCENARIOSESSION FOREIGN KEY (scenario_session_id) REFERENCES sciconum_scenario_session_entity (id);
 
 ALTER TABLE sciconum_chatter_entity
 	ADD CONSTRAINT FK_SCICONUMCHATTERENTITY_ON_LEARNER FOREIGN KEY (learner_id) REFERENCES users (id);
 
 ALTER TABLE sciconum_chatter_entity
-	ADD CONSTRAINT FK_SCICONUMCHATTERENTITY_ON_PEERING FOREIGN KEY (peering_id) REFERENCES sciconum_peering_entity (id);
+	ADD CONSTRAINT FK_SCICONUMCHATTERENTITY_ON_PEERING FOREIGN KEY (peering_id) REFERENCES sciconum_chat_peering_entity (id);
 
-ALTER TABLE sciconum_learner_session_entity
-	ADD CONSTRAINT FK_SCICONUMLEARNERSESSIONENTITY_ON_GLOBALSESSION FOREIGN KEY (global_session_id) REFERENCES sciconum_session_entity (id);
+ALTER TABLE sciconum_judge_peering_entity
+	ADD CONSTRAINT FK_SCICONUMJUDGEPEERINGENTITY_ON_LEARNERSESSION FOREIGN KEY (learner_session_id) REFERENCES sciconum_learner_session_entity (id);
 
 ALTER TABLE sciconum_learner_session_entity
 	ADD CONSTRAINT FK_SCICONUMLEARNERSESSIONENTITY_ON_LEARNER FOREIGN KEY (learner_id) REFERENCES users (id);
 
-ALTER TABLE sciconum_peering_entity
-	ADD CONSTRAINT FK_SCICONUMPEERINGENTITY_ON_LEARNER FOREIGN KEY (learner_id) REFERENCES users (id);
+ALTER TABLE sciconum_learner_session_entity
+	ADD CONSTRAINT FK_SCICONUMLEARNERSESSIONENTITY_ON_SCENARIOSESSION FOREIGN KEY (scenario_session_id) REFERENCES sciconum_scenario_session_entity (id);
 
-ALTER TABLE sciconum_peering_entity
-	ADD CONSTRAINT FK_SCICONUMPEERINGENTITY_ON_SESSION FOREIGN KEY (session_id) REFERENCES sciconum_session_entity (id);
+ALTER TABLE sciconum_scenario_session_entity
+	ADD CONSTRAINT FK_SCICONUMSCENARIOSESSIONENTITY_ON_ASSIGNMENT FOREIGN KEY (assignment_id) REFERENCES assignment_entity (id);
 
-ALTER TABLE sciconum_session_entity
-	ADD CONSTRAINT FK_SCICONUMSESSIONENTITY_ON_ASSIGNMENT FOREIGN KEY (assignment_id) REFERENCES assignment_entity (id);
+ALTER TABLE sciconum_scenario_session_entity
+	ADD CONSTRAINT FK_SCICONUMSCENARIOSESSIONENTITY_ON_SEQUENCE FOREIGN KEY (sequence_id) REFERENCES sequence_entity (id);
 
-ALTER TABLE sciconum_session_entity
-	ADD CONSTRAINT FK_SCICONUMSESSIONENTITY_ON_SEQUENCE FOREIGN KEY (sequence_id) REFERENCES sequence_entity (id);
+ALTER TABLE sciconum_judge_peering_entity_responses
+	ADD CONSTRAINT fk_scijudpeeentres_on_response_entity FOREIGN KEY (responses_id) REFERENCES response_entity (id);
 
-ALTER TABLE sciconum_peering_entity_responses
-	ADD CONSTRAINT fk_scipeeentres_on_response_entity FOREIGN KEY (responses_id) REFERENCES response_entity (id);
-
-ALTER TABLE sciconum_peering_entity_responses
-	ADD CONSTRAINT fk_scipeeentres_on_sciconum_judge_peering_entity FOREIGN KEY (sciconum_judge_peering_entity_id) REFERENCES sciconum_peering_entity (id);
+ALTER TABLE sciconum_judge_peering_entity_responses
+	ADD CONSTRAINT fk_scijudpeeentres_on_sciconum_judge_peering_entity FOREIGN KEY (sciconum_judge_peering_entity_id) REFERENCES sciconum_judge_peering_entity (id);
