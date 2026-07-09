b5d036daf964ca65f8f66d955990fbd123a11cf6
John Tranier <tranier@gmail.com>
2026-07-09 14:51:15 +0200

    fix: Fix the condition for empty answer with multiple choice question

--- frontend/app/components/ClosedQuestion.vue
+++ frontend/app/components/ClosedQuestion.vue
@@ -79,14 +79,14 @@ const schema = computed(() => {
 })
 
 const response = reactive<Output>({
-	answer: null,
+	answer: question.multiple ? [] : null,
 	selfExplanation: null,
 	confidenceDegree: null,
 })
 
 const options = computed(() => question.choices.map((c, i) => ({ label: c, value: i })))
 const state = computed(() => ({
-	answer: response.answer !== null,
+	answer: (question.multiple && response.answer.length > 0) || (!question.multiple && response.answer !== null),
 	selfExplanation: !requestSelfExplanation || response.selfExplanation !== null,
 	confidenceDegree: !confidenceDegreeOptions || response.confidenceDegree !== null,
 }))
