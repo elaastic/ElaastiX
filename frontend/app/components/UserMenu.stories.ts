8fafe3ab348040fec1c3e22acc296a686634c7e5
John Tranier <tranier@gmail.com>
2026-07-23 18:06:28 +0200

    fix: Use "satisfies" instead of "as" to ensure type validation

--- frontend/app/components/UserMenu.stories.ts
+++ frontend/app/components/UserMenu.stories.ts
@@ -57,7 +57,7 @@ export const Login: Story = {
 					lastname: 'Silvestre',
 					email: 'franck.silvestre.com',
 					roles: ['ADMIN', 'WRITER'],
-				} as UserAccountDto
+				} satisfies UserAccountDto
 			},
 			template: '<story />',
 		}),
