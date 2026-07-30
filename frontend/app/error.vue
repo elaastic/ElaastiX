a4f6a3b51dff5f799f76a4a995b76cdd3e30e259
Yann L <yannlacaze31@gmail.com>
2026-07-30 15:43:40 +0200

    feat: make the dashboard of a sequence accessible only to his creator
    # Conflicts:
    #	frontend/i18n/locales/en-GB.json
    #	frontend/i18n/locales/fr-FR.json

---
+++ frontend/app/error.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import type { NuxtError } from '#app'
+
+const { error } = defineProps<{ error: NuxtError }>()
+</script>
+
+<template>
+	<NuxtLayout name="default">
+		<UDashboardPanel id="Error">
+			<template #header>
+				<UDashboardNavbar title="Error">
+					<template #leading>
+						<UDashboardSidebarCollapse />
+					</template>
+				</UDashboardNavbar>
+			</template>
+
+			<template #body>
+				<UError
+					:error="error"
+					class="w-full h-full"
+					icon="i-lucide-circle-x"
+				/>
+			</template>
+		</UDashboardPanel>
+	</NuxtLayout>
+</template>
