e1b13bdb275eb1e901bf791178a7a14f2a7ac985
Yann L <yannlacaze31@gmail.com>
2026-08-11 11:32:55 +0200

    fix: fix a bug on startup du to the processing on ScenarioSessionCreator
    When the learner was joining a session in the seeder, the scenarioSession wasnt created yet. Got help from DeepSeek V4 Flash.

--- server/src/main/kotlin/org/elaastix/server/core/infrastructure/seed/AssignmentSeeder.kt
+++ server/src/main/kotlin/org/elaastix/server/core/infrastructure/seed/AssignmentSeeder.kt
@@ -49,7 +49,7 @@ class AssignmentSeeder(
 			entity = AssignmentEntity(
 				displayName = "Assignment 1",
 				sequences = mutableListOf(sequenceSeeder.sequence1, sequenceSeeder.sequence2, sequenceSeeder.sequence3),
-				participants = mutableSetOf(userSeeder.student1, userSeeder.student2),
+				participants = mutableSetOf(),
 				owner = userSeeder.franck,
 			).apply {
 				this.creator = userSeeder.franck
@@ -61,7 +61,7 @@ class AssignmentSeeder(
 			entity = AssignmentEntity(
 				displayName = "Assignment 2",
 				sequences = mutableListOf(sequenceSeeder.sequenceTest),
-				participants = mutableSetOf(userSeeder.student2, userSeeder.student3),
+				participants = mutableSetOf(),
 				owner = userSeeder.john,
 			).apply {
 				this.creator = userSeeder.john
