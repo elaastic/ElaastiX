752ef0e5755036bc1b3da193b8fd530e1c854b5f
John Tranier <john.tranier@ticetime.com>
2026-08-13 10:02:23 +0200

    refactor: Be more explicit about isSequenceAssociated
    `!data` suggest there is no data, which is quite different from an explicit `false` response.

--- frontend/app/middleware/sequence.ts
+++ frontend/app/middleware/sequence.ts
@@ -2,7 +2,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
 	const uuid = to.params.uuid as string
 
 	const { $api, $i18n } = useNuxtApp()
-	const data = await $api(
+	const isSequenceAssociated = await $api(
 		`/v1/player/org.elaastix.engine.isSequenceAssociated`,
 		{
 			method: 'POST',
@@ -12,7 +12,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
 		},
 	)
 
-	if (!data) {
+	if (isSequenceAssociated === false) {
 		return showError({
 			status: 403,
 			statusText: $i18n.t('sequence.error.forbidden'),
