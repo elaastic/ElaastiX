a4afe51d78f4ff4af3a2f9181b0ac1fb1bbec822
John Tranier <john.tranier@ticetime.com>
2026-09-03 09:41:56 +0200

    feat: Add SequenceLearnerView component
    Extracted from https://github.com/elaastic/ElaastiX/pull/277

---
+++ frontend/app/components/SequenceLearnerView.stories.ts
@@ -0,0 +1,56 @@
+/*
+ * Elaastic / ElaastiX - formative assessment system
+ * Copyright (C) 2019  Université de Toulouse and Université Toulouse Capitole.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import type { components } from '#open-fetch-schemas/api'
+import SequenceLearnerView from './SequenceLearnerView.vue'
+import type { Meta, StoryObj } from '@nuxtjs/storybook'
+
+const meta = {
+	title: 'Sequence Learner View',
+	component: SequenceLearnerView,
+	tags: ['autodocs'],
+} satisfies Meta<typeof SequenceLearnerView>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const question = {
+	id: '0000000000000000000000000',
+	$type: 'ClosedQuestion',
+	statement: {
+		$type: 'Markdown',
+		content: 'What is the best multipurpose operating system?',
+	},
+	multiple: false,
+	choices: [
+		{ $type: 'PlainText', content: 'Linux' },
+		{ $type: 'PlainText', content: 'Windows' },
+		{ $type: 'PlainText', content: 'Darwin (macOS)' },
+		{ $type: 'PlainText', content: 'OpenBSD' },
+	],
+} satisfies
+| components['schemas']['ClosedQuestionStatementDto']
+| components['schemas']['OpenQuestionStatementDto']
+| undefined
+
+export const Question: Story = {
+	args: {
+		question: question,
+	},
+}
